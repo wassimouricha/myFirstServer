27b85a6a57dca9e6c760c1fdceede157fe4e6347
wassim <wbouricha5@gmail.com>
2022-01-06 11:58:36 +0100

    go to localhost 8070 and add ?name=yourname

--- indexus.js
+++ indexus.js
@@ -1,22 +1,37 @@
 let http = require('http')
 let fs = require('fs')
 
+let url = require('url')
+
 let server = http.createServer()
 
 server.on('request' , (request,response) => {
 
-    fs.readFile('index.html', (err,data) => {
-        if (err) throw err
+    response.writeHead(200)
+    let  query =  url.parse(request.url,true).query
+    if (query.name === undefined) {
+        response.write('bonjour anonyme')
+    } else {
+        response.write('bonjour ' +  query.name)
+    }
+    response.end()
 
-        response.writeHead(200, {
-            'content-type': 'text/html; charset=utf-8'
-        } )
 
-        response.end(data)
-        })
+    // fs.readFile('index.html', (err,data) => {
+    //     if (err) {
+    //         response.writeHead(404)
+    //         response.end("ce fichier n'existe pas")
+    //     }
+    //     else{
+    //     response.writeHead(200, {
+    // //         'content-type': 'text/html; charset=utf-8'
+    // //     } )
+
+    // //     response.end(data)}
+    // //     })
 
     })
 
 
 
-server.listen(8070)
+    server.listen(8070)
